58e41830491f672244c519db4692167ea3f3a6f6
yurubahmed <yurubiftin2002@gmail.com>
2023-02-08 12:24:32 -0500

    passed all tests

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+    # Class body
+  
+    # Instance Method Definition
+    def bark
+      puts "Woof!"
+    end
+    def sit
+        puts "The Dog is sitting"
+    end
+  end
+  fido = Dog.new
+  fido.bark
+  fido.sit
+
+  
+  
